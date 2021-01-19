83d6ca7b9dab6159aad2e885c6f27f7c6d5337ad
TVHOP <74019075+TVHOP@users.noreply.github.com>
2021-01-19 13:37:40 -0600

    BOUNCY BALLS

---
+++ bouncy ball/sketch.js
@@ -0,0 +1,53 @@
+// Bouncy Ball
+// A M
+// 1/19/21
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let theBouncingBalls = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background('black');
+  moveBall();
+  displayBall();
+}
+
+function mousePressed() {
+  let ball = {
+    x: mouseX,
+    y: mouseY,
+    diameter: random(25, 100),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    theColour: color(random(255), random(255), random(255), random(255)),
+  };
+  theBouncingBalls.push(ball);
+}
+
+function moveBall() {
+  for (let ball of theBouncingballs) {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+  
+    if (ball.x + ball.diameter/2 >= width || ball.x - ball.diameter/2 <= 0) {
+      ball.dx *= -1;
+    }
+  
+    if (ball.y + ball.diameter/2 >= height || ball.y - ball.diameter/2 <= 0) {
+      ball.dy *= -1;
+  }
+
+  }
+}
+function displayBall() {
+  for (let ball of theBouncingBalls) {
+    noStroke();
+    fill(theBouncingBalls.theColour)
+    ellipse(ball.x, ball.y, ball.diameter, ball.diameter);
+  }
+}
