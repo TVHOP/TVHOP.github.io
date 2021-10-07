f55664f57caf776d004097bcfcb3f7821523c367
TVHOP <74019075+TVHOP@users.noreply.github.com>
2021-10-07 10:53:25 -0600

    floating balls perlin noise

---
+++ Ball Array/sketch.js
@@ -0,0 +1,52 @@
+let ballArray = [];
+
+let i;
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i=0; i<100; i++) {
+    spawnBall();
+
+  }
+}
+
+function draw() {
+  background(220);
+
+  for (let myBall of ballArray) {
+    noStroke();
+    fill(myBall.theColor);
+    myBall.x = noise(myBall.time) * width;
+    myBall.y = noise(myBall.time+100) * height;
+    circle(myBall.x, myBall.y, myBall.radius*2);
+    myBall.time += 0.003;
+  }
+}
+
+
+
+
+  
+
+function spawnBall() {
+  let myBall = {
+    radius: random(10,30),
+    x: random(width),
+    y: random(height),
+    time: random(1000),
+    theColor: color(random(255), random(255), random(255), random(255)),
+
+
+  };
+  ballArray.push(myBall);
+}
+
+function mousePressed() {
+  for (i = 0; i < 5; i++) {
+    spawnBall();
+  }
+  
+}
+
