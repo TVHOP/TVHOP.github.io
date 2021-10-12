215909e687d583fb923cc36b4054e6d89a807e4b
TVHOP <74019075+TVHOP@users.noreply.github.com>
2021-10-12 12:11:45 -0600

    did things
    change color on click

--- Grid Demo/sketch.js
+++ Grid Demo/sketch.js
@@ -14,6 +14,21 @@ function draw() {
   displayGrid();
 }
 
+function mousePressed() {
+  let cellWidth = width/gridSize;
+  let cellHeight = height/gridSize;
+
+  let cellX = Math.floor(mouseX/cellWidth);
+  let cellY = Math.floor(mouseY/cellHeight);
+
+  if(grid[cellY][cellX] === 1) {
+    grid[cellY][cellX] = 0;
+  }
+  else if(grid[cellY][cellX] === 0) {
+    grid[cellY][cellX] = 1;
+  }
+}
+
 function displayGrid() {
   let cellWidth = width/gridSize;
   let cellHeight = height/gridSize;
@@ -65,6 +80,6 @@ function keyPressed() {
   }
 }
 
-function mousePressed() {
-  setup();
-}
+// function mousePressed() {
+//   setup();
+// }
